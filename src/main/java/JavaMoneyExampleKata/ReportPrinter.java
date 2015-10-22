133b74cf76f6c2f5d9431268b317a56e7b61a22b
Daniele Megna <daniele.megna@xpeppers.com>
2015-10-22 17:16:14 +0200

    Refactoring on ReportPrinter

--- src/main/java/JavaMoneyExampleKata/ReportPrinter.java
+++ src/main/java/JavaMoneyExampleKata/ReportPrinter.java
@@ -4,22 +4,44 @@ import java.util.Iterator;
 
 public class ReportPrinter {
 
-	private static final String REPORT_HEADER = "Instrument\t\tShares\t\tPrice\t\tTotal\n";
+	private StringBuilder printResult = null;
 
 	public String print(Report report) {
-		StringBuilder print = new StringBuilder();
+		initNewPrint();
 		
-		print.append(REPORT_HEADER);
+		appendHeader();
+		appendReportItemsRows(report);
+		appendFooterTotal(report);
+		
+		return printResultString();
+	}
 
+	private void initNewPrint() {
+		printResult = new StringBuilder();
+	}
+
+	private void appendHeader() {
+		printResult.append("Instrument\t\tShares\t\tPrice\t\tTotal\n");
+	}
+	
+	private void appendReportItemsRows(Report report) {
 		Iterator<ReportItem> itemsIterator = report.items();
-		while(itemsIterator.hasNext()) {
-			ReportItem item = itemsIterator.next();
-			print.append(item.instrument() + "\t\t" + item.shares() + "\t\t" + item.price() + "\t\t" + item.total() + "\n");
-		}
-		
-		print.append("\t\t\t\t\t\tTotal\t\t" + report.total());
-		
-		return print.toString();
+		while(itemsIterator.hasNext())
+			appendReportItemRow(itemsIterator.next());
+	}
+	
+	private void appendReportItemRow(ReportItem item) {
+		printResult.append(
+			item.instrument() + "\t\t" + item.shares() + "\t\t" + item.price() + "\t\t" + item.total() + "\n"
+		);
+	}
+	
+	private void appendFooterTotal(Report report) {
+		printResult.append("\t\t\t\t\t\tTotal\t\t" + report.total());
+	}
+	
+	private String printResultString() {
+		return printResult.toString();
 	}
 
 }
